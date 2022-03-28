108b6e1f0880d6e2503b83d6acd5196a7a421c8e
Dipirona537 <dipirona28@gmail.com>
2022-03-27 23:48:55 -0400

    sla2

--- src/com/dipirona/modpackinstaller/download_mods.java
+++ src/com/dipirona/modpackinstaller/download_mods.java
@@ -4,90 +4,90 @@ public class download_mods {
 
     public static String backup() {
         String comando = "mkdir %APPDATA%\\.minecraft\\Backupmodpack";
-        String str = "cmd.exe /c start cmd /c" + comando;
-        return str;
+        return "cmd.exe /c start cmd /c" + comando;
     }
 
     public static String mover() {
         String comando = "move %APPDATA%\\.minecraft\\mods %APPDATA%\\.minecraft\\Backupmodpack\\mods" + data_atual();
-        String str = "cmd.exe /c start cmd /c" + comando;
-        return str;
+        return "cmd.exe /c start cmd /c" + comando;
     }
 
     public static String mover2() {
         String comando = "move %APPDATA%\\.minecraft\\config %APPDATA%\\.minecraft\\Backupmodpack\\config" + data_atual();
-        String str = "cmd.exe /c start cmd /c" + comando;
-        return str;
+        return "cmd.exe /c start cmd /c" + comando;
     }
 
     public static String mover3() {
         String comando = "move %APPDATA%\\.minecraft\\kubejs %APPDATA%\\.minecraft\\Backupmodpack\\kubejs" + data_atual();
-        String str = "cmd.exe /c start cmd /c" + comando;
-        return str;
+        return "cmd.exe /c start cmd /c" + comando;
     }
 
     public static String mover4() {
         String comando = "move %APPDATA%\\.minecraft\\resources %APPDATA%\\.minecraft\\Backupmodpack\\resources" + data_atual();
-        String str = "cmd.exe /c start cmd /c" + comando;
-        return str;
+        return "cmd.exe /c start cmd /c" + comando;
     }
 
+    public static String mover5() {
+        String comando = "move %APPDATA%\\.minecraft\\defaultconfigs %APPDATA%\\.minecraft\\Backupmodpack\\defaultconfigs" + data_atual();
+        return "cmd.exe /c start cmd /c" + comando;
+    }
 
-    public static String comando1() {
-        String comando = "ipconfig";
-        String str = "cmd.exe /c start cmd /c" + comando;
-        return str;
+    public static String mover6() {
+        String comando = "move %APPDATA%\\.minecraft\\options.txt %APPDATA%\\.minecraft\\Backupmodpack\\options.txt";
+        return "cmd.exe /c start cmd /c" + comando;
+    }
+
+    public static String mover7() {
+        String comando = "move %APPDATA%\\.minecraft\\optionsof.txt %APPDATA%\\.minecraft\\Backupmodpack\\optionsof.txt";
+        return "cmd.exe /c start cmd /c" + comando;
     }
 
 
     public static String comando2() {
         String comando = "color 2";
-        String str = "cmd /c start cmd.exe /K" + comando;
-        return str;
+        return "cmd /c start cmd.exe /k" + comando;
     }
 
     public static String comando3() {
         String comando = "wget";
-        String str = "cmd /c start cmd.exe /K" + comando;
-        return str;
+        return "cmd /c start cmd.exe /k" + comando;
     }
 
     public static String data_atual() {
-        String data = String.valueOf(java.time.LocalDate.now());
-        return data;
+        return String.valueOf(java.time.LocalDate.now());
     }
 
     public static String modpack1() {
         String comando = "wget -c https://www.dropbox.com/s/dxdwamhh7icg6mn/mods%20ftbinfinityanniversary.rar?dl=1 -O mods.rar";
-        String str = "cmd /c start cmd.exe /c" + comando;
-        return str;
-    }
-
-    public static String movermods()
-    {
-        String comando = "move mods.rar temp";
-        String str = "cmd /c start cmd.exe /c" + comando;
-        return str;
+        return "cmd /c start cmd.exe /c" + comando;
     }
 
     public static String descompactar() {
-        String comando = "unRAR.exe x mods.rar temp";
-        String str = "cmd /c start cmd.exe /c" + comando;
-        return str;
+        String comando = "unRAR.exe x mods.rar %appdata%\\.minecraft";
+        return "cmd /c start cmd.exe /c" + comando;
     }
 
-    public static String temp()
-    {
-        String comando = "mkdir temp\\";
-        String str = "cmd /c start cmd.exe /c" + comando;
-        return str;
-    }
-
-    public static String deletar()
-    {
+    public static String deletar() {
         String comando = "del mods.rar";
-        String str = "cmd /c start cmd.exe /c" + comando;
-        return str;
+        return "cmd /c start cmd.exe /c" + comando;
+    }
+
+    public static String forge_download()
+    {
+        String comando = "wget -c https://maven.minecraftforge.net/net/minecraftforge/forge/1.16.5-36.2.33/forge-1.16.5-36.2.33-installer.jar -O forge1.16.5-36.2.33.jar";
+        return "cmd /c start cmd.exe /c" + comando;
+    }
+
+    public static String forge_install()
+    {
+        String comando = "java -jar forge1.16.5-36.2.33.jar";
+        return "cmd /c start cmd.exe /c" + comando;
+    }
+
+    public static String forge_del()
+    {
+        String comando = "del forge1.16.5-36.2.33.jar";
+        return "cmd /c start cmd.exe /c" + comando;
     }
 
 
--- src/com/dipirona/modpackinstaller/interfacegrafica.java
+++ src/com/dipirona/modpackinstaller/interfacegrafica.java
@@ -2,7 +2,9 @@ package com.dipirona.modpackinstaller;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class interfacegrafica {
         public static void main(String[] args)
@@ -12,9 +14,15 @@ public class interfacegrafica {
             String mover2 = download_mods.mover2();
             String mover3 = download_mods.mover3();
             String mover4 = download_mods.mover4();
+            String mover5 = download_mods.mover5();
+            String mover6 = download_mods.mover6();
+            String mover7 = download_mods.mover7();
             String modpack1 = download_mods.modpack1();
             String descompactar = download_mods.descompactar();
-            String temp = download_mods.temp();
+            String deletar = download_mods.deletar();
+            String forge_download = download_mods.forge_download();
+            String forge_install = download_mods.forge_install();
+            String forge_del = download_mods.forge_del();
 
             JFrame iconef = new JFrame();
             Image icone = Toolkit.getDefaultToolkit().getImage("icone.png");
@@ -33,20 +41,48 @@ public class interfacegrafica {
                     b1.setFont(new Font("Arial", Font.BOLD, 10));
                     iconef.add(b1);
                 }
-                String comando1 = download_mods.comando1();
                 b1.addActionListener(e -> {
                     try {
-                        Runtime.getRuntime().exec(backup).waitFor();
-                        Runtime.getRuntime().exec(mover).waitFor();
-                        Runtime.getRuntime().exec(mover2).waitFor();
-                        Runtime.getRuntime().exec(mover3).waitFor();
-                        Runtime.getRuntime().exec(mover4).waitFor();
-                        Runtime.getRuntime().exec(temp).waitFor();
-                        Runtime.getRuntime().exec(modpack1).waitFor();
-                        Runtime.getRuntime().exec(descompactar).waitFor();
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    } catch (InterruptedException ex) {
+                        Runtime.getRuntime().exec(backup);
+                        Runtime.getRuntime().exec(mover);
+                        Runtime.getRuntime().exec(mover2);
+                        Runtime.getRuntime().exec(mover3);
+                        Runtime.getRuntime().exec(mover4);
+                        Runtime.getRuntime().exec(mover5);
+                        Runtime.getRuntime().exec(mover6);
+                        Runtime.getRuntime().exec(mover7);
+                        Process p9 = Runtime.getRuntime().exec(modpack1);
+                        BufferedReader stdOut =new BufferedReader(new InputStreamReader(p9.getInputStream()));
+                        while ((stdOut.readLine())!=null){
+                            p9.waitFor();
+                        }
+                        Process p10 = Runtime.getRuntime().exec(descompactar);
+                        BufferedReader stdOut1 =new BufferedReader(new InputStreamReader(p10.getInputStream()));
+                        while ((stdOut1.readLine())!=null){
+                            p10.waitFor();
+                        }
+                        Process p11 = Runtime.getRuntime().exec(deletar);
+                        BufferedReader stdOut2 =new BufferedReader(new InputStreamReader(p11.getInputStream()));
+                        while ((stdOut2.readLine())!=null){
+                            p11.waitFor();
+                        }
+                        Process p12 = Runtime.getRuntime().exec(forge_download);
+                        BufferedReader stdOut3 =new BufferedReader(new InputStreamReader(p12.getInputStream()));
+                        while ((stdOut3.readLine())!=null){
+                            p12.waitFor();
+                        }
+                        Process p13 = Runtime.getRuntime().exec(forge_install);
+                        BufferedReader stdOut4 =new BufferedReader(new InputStreamReader(p13.getInputStream()));
+                        while ((stdOut4.readLine())!=null){
+                            p13.waitFor();
+                        }
+                        Process p14 = Runtime.getRuntime().exec(forge_del);
+                        BufferedReader stdOut5 =new BufferedReader(new InputStreamReader(p14.getInputStream()));
+                        while ((stdOut5.readLine())!=null){
+                            p14.waitFor();
+                        }
+                        JOptionPane.showMessageDialog(null, "Modpack instalado com sucesso!");
+                    } catch (IOException | InterruptedException ex) {
                         ex.printStackTrace();
                     }
                 });
